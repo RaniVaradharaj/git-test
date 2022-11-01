0dbef03592ae9a0bac5635b10fc77b35350e8511
RaniVaradharaj <ranidvhsr31@gmail.com>
2022-11-02 01:32:45 +0530

    message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'; //File
+import App from './App'; //File ..
 
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
